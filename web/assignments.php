ead523af336d5e9d82561c929420cf9b0e6067fc
russjenni@gmail.com <russjenni@gmail.com>
2019-09-27 22:58:12 -0700

    added hello world page

---
+++ web/assignments.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <script src="homepagejs.js"></script>
+        <link rel="stylesheet" type="text/css" href="homepagecss.css">
+        <title>Assignments</title>
+    </head>
+    
+    <body>
+        <p>Coming Soon!</p>
+    </body>
+
+</html>
---
+++ web/personalHomepage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <script src="homepagejs.js"></script>
+        <link rel="stylesheet" type="text/css" href="homepagecss.css">
+        <title>Homepage</title>
+    </head>
+    
+    <body>
+        <a href="assignments.php">Go to Assignments</a>
+    </body>
+
+</html>
