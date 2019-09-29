3d650208fe67ed2808b3869e72663e6bfa74b9e0
russjenni@gmail.com <russjenni@gmail.com>
2019-09-28 19:30:08 -0700

    added personalHomepage

--- web/assignments.php
+++ web/assignments.php
@@ -9,13 +9,13 @@
     
     <body>
         <?php
-           date_default_timezone_set('PHP');
+           date_default_timezone_set('America/Los_Angeles');
            $date = date('m-d-y');
            $time = date('H:i:s');
            $day = date('l');
-           echo 'The current day is'.$day;
-           echo 'The current date is'.$date;
-           echo 'The current time is'.$time;
+           echo 'The current day is '.$day;
+           echo '. The current date is'.$date;
+           echo '. The current time is'.$time;
         ?>
         
         <p>Coming Soon</p>
