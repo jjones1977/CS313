457b0f0726efa24af8e03e002d71fdac3a813e2b
russjenni@gmail.com <russjenni@gmail.com>
2019-09-28 19:10:57 -0700

    added personalHomepage

--- web/assignments.php
+++ web/assignments.php
@@ -2,13 +2,22 @@
 <html>
     
     <head>
-        <script src="homepagejs.js"></script>
         <link rel="stylesheet" type="text/css" href="homepagecss.css">
-        <title>Assignments</title>
+        <title>Homepage</title>
     </head>
     
     <body>
-        <p>Coming Soon!</p>
+        <?php
+           date_default_timezone_set('PHP');
+           $date = date('m-d-y');
+           $time = date('H:i:s');
+           $day = date('l');
+           echo 'The current day is'.$day;
+           echo 'The current date is'.$date;
+           echo 'The current time is'.$time;
+        ?>
+        
+        <p>Coming Soon</p>
     </body>
-
+    
 </html>
--- web/personalHomepage.php
+++ web/personalHomepage.php
@@ -2,13 +2,48 @@
 <html>
     
     <head>
-        <script src="homepagejs.js"></script>
         <link rel="stylesheet" type="text/css" href="homepagecss.css">
         <title>Homepage</title>
     </head>
     
     <body>
-        <a href="assignments.php">Go to Assignments</a>
+        <a class="pLink" href="assignments.html"><p>Go to Assignments</p></a>
+        
+        
+        <div class="divContainer1">
+           
+           <div id="divImg1">Come</div>
+            <img src="Dice3.jpg" alt="Dice">
+        </div>
+        <div class="divContainer2">
+           <img src="Dice3.jpg" alt="Dice">
+           <div id="divImg2">Play</div> 
+        </div>
+        <div class="divContainer3">
+            <img src="Dice3.jpg" alt="Dice">
+            <div id="divImg3">Bunco</div>
+        </div>
+        <h1>Bunco!</h1> 
+        
+        <h2>What is Bunco?</h2>
+            <div class="div2">
+                Bunco is a social dice game, traditionally played with 12 players who are divided into three tables with four players at each table. But really, almost any number can play. And if the number of players is not divisible by 4, tack on a ghost player, or two.
+            </div>
+        
+        <h2>The Basics</h2>
+        <div class="div1">
+            <ul class="a">
+            <li>A game of Bunco is played in two to four sets, with six rounds in each set.</li>
+            <li>A player at the head table rings a bell to signal the beginning and end of each round.</li>
+            <li>During the game, players at each table take turns rolling three dice to try and earn points. All tables play simultaneously.</li>
+            <li>During each round, players attempt to roll the same number as the number of the round.</li>
+            <li>For every number rolled that matches the round number, one point is awarded to that player.</li>
+            <li>A player keeps rolling until they score no points. The scorekeeper then records the score they earned onto the table tally as the temporary team score. Then the dice are passed to the player on the left.</li>
+            <li>When the head table has earned 21 points, the round is over. The head table rings a bell to signal that the round has ended.</li>
+            <li>At the end of every round, players change partners and/or tables. </li>
+            <li>At the end of a game, there are typically multiple "winners." The player(s) with the most Buncos, wins, losses, etc, may earn prizes for their successes.</li>
+            </ul>
+        </div>
     </body>
 
 </html>
